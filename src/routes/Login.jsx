53f0191cd0382b139e3821b93caa4192735aafe0
divad <davidoluwaseyitan@gmail.com>
2022-10-09 23:15:54 +0100

    commit11

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,26 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FormControl, TextField } from '@mui/material'
+import { Link } from 'react-router-dom'
 const Login = () => {
   return (
-    <Container>
-        <div className="title">
-            <FormControl>
+    <Container className='flex'>
+        <div className="wrapper grid">
+
+            <div className="title">
+                <h1 className="page-title">SIGN IN</h1>
+            </div>
+
+            <FormControl className='form grid'>
 
                 <TextField
-                    required
-                    id="filled-required"
-                    label="Required"
-                    defaultValue="Hello World"
-                    variant="filled"
+                    required 
+                    className='input'
+                    id="username"
+                    label="Username"
+                    variant="standard"
+                    size="small"
                 />
 
                 <TextField
-                    id="outlined-password-input"
-                    label="Password"
+                    required 
+                    id="password"
+                    className='input'
                     type="password"
+                    label="Password"
+                    variant="standard"
                     autoComplete="current-password"
+                    size="small"
                 />
+
+                <button className="button">Sign In</button>
+
+                <Link to='' className='link'>DO YOU NOT REMEMBER THE PASSWORD?</Link>
+                <Link to='/register' className='link'>CREATE A NEW ACCOUNT</Link>
 
             </FormControl>
         </div>
@@ -28,7 +44,69 @@ const Login = () => {
   )
 }
 const Container = styled.div`
-
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+        center;
+    background-size: cover;
+    align-items: center;
+    justify-content: center;
+    .wrapper{
+        width: 75%;
+        padding: 1.5rem;
+        background: var(--white-color);
+    }
+    .form{
+        gap:1rem;
+    }
+    .page-title{
+        font-size:var(--h3-size);
+        font-weight:700;
+        text-align:center;
+    }
+    .input{
+        font-size:var(--font-size-sm) !important;
+        color:var(--dark-text) !important;
+    }
+    .link{
+        font-size:.65rem;
+        font-weight:700;
+        text-align:center;
+        color:var(--dark-text2);
+        font-family:var(--bdy-font-family-alt);
+        :hover{
+            color:var(--dark-text);
+        }
+    }
+    .button{
+        padding:.25rem .75rem;
+        text-align:center;
+        font-weight: var(--font-semibold);
+        color: var(--white-text);
+        text-transform: uppercase;
+        cursor: pointer;
+        background:var(--skin-color);
+        border: 1px solid var(--skin-color);
+        box-shadow: var(--box-shadow);
+        max-width:80%;
+        margin:auto;
+        :hover{
+            background:transparent;
+            color:var(--dark-text);
+        }
+    }
+    @media (min-width:960px){
+        .wrapper{
+            width: 30%;
+        }
+        .button{
+            max-width:40%;
+        }
+    }
 `
 
 export default Login
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FormControl, TextField } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
   return (
-    <Container>
-        <div className="wrapper">
+    <Container className='flex'>
+        <div className="wrapper grid">
 
             <div className="title">
                 <h1 className="page-title">CREATE AN ACCOUNT</h1>
             </div>
 
-            <FormControl>
+            <FormControl className='form grid'>
+                <div className="name flex">
 
                 <TextField
                     required
@@ -19,23 +21,30 @@ const Register = () => {
                     label="Name"
                     variant="standard"
                     size="small"
+                    className='input'
                 />
                 <TextField
-                    required
+                    required 
+                    className='input'
                     id="lastName"
                     label="Last name"
                     variant="standard"
                     size="small"
                 />
+
+                </div>
+                
                 <TextField
-                    required
+                    required 
+                    className='input'
                     id="email"
                     label="Email"
                     variant="standard"
                     size="small"
                 />
                 <TextField
-                    required
+                    required 
+                    className='input'
                     id="username"
                     label="Username"
                     variant="standard"
@@ -43,25 +52,34 @@ const Register = () => {
                 />
 
                 <TextField
+                    required 
                     id="password"
+                    className='input'
                     type="password"
                     label="Password"
+                    variant="standard"
                     autoComplete="current-password"
                     size="small"
                 />
                 <TextField
+                    required 
                     id="confirm-password"
+                    className='input'
                     type="password"
                     label="Confirm password"
                     autoComplete="current-password"
+                    variant="standard"
                     size="small"
                 />
 
-                <p className="agreement"> By creating an account, I consent to the processing of my personal
-                    data in accordance with the <b>PRIVACY POLICY</b>
+                <p className="agreement"> 
+                    By creating an account, I consent to the processing of my personal
+                    data in accordance with the <span>PRIVACY POLICY</span>
                 </p>
                 
-                <button className="">Create Account</button>
+                <button className="button">Create Account</button>
+
+                <Link to='/sign-in' className='link'>SIGN IN</Link>
 
             </FormControl>
 
@@ -71,6 +89,84 @@ const Register = () => {
   )
 }
 const Container = styled.div`
+    justify-content:center;
+    align-items:center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+        center;
+    background-size: cover;
 
+    .wrapper{
+        width: 80%;
+        padding: 1.5rem;
+        background: var(--white-color);
+    }
+    .form{
+        gap:.5rem;
+    }
+    .page-title{
+        font-size:var(--h3-size);
+        font-weight:700;
+        text-align:center;
+    }
+    .name{
+        gap:3rem;
+    }
+    .input{
+        font-size:var(--font-size-sm) !important;
+        color:var(--dark-text) !important;
+    }
+    .agreement{
+        font-size:var(--font-size-sm);
+        font-weight:500;
+        text-align:center;
+        color:var(--dark-text2);
+        span{
+            font-size:var(--font-size-sm);
+            font-weight:700;
+            color:var(--dark-text);
+        }
+    }
+    .link{
+        font-size:.65rem;
+        font-weight:700;
+        text-align:center;
+        color:var(--dark-text2);
+        font-family:var(--bdy-font-family-alt);
+        margin-top:.15rem;
+        :hover{
+            color:var(--dark-text);
+        }
+    }
+    .button{
+        padding:.5rem;
+        text-align:center;
+        font-weight: var(--font-semibold);
+        color: var(--white-text);
+        text-transform: uppercase;
+        cursor: pointer;
+        background:var(--skin-color);
+        border: 1px solid var(--skin-color);
+        box-shadow: var(--box-shadow);
+        max-width:80%;
+        margin:auto;
+        :hover{
+            background:transparent;
+            color:var(--dark-text);
+        }
+    }
+    @media (min-width:960px){
+        .wrapper{
+            width: 40%;
+        }
+        .button{
+            max-width:40%;
+        }
+    }
 `
 export default Register
